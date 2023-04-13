858fd62670b029ea6a7f98a95b308db31e5ddf7c
Matthew <matthew1@enigmalabs.io>
2023-04-13 11:52:52 +0100

    Initial Commit

---
+++ Lego/LegoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegoApp.swift
+//  Lego
+//
+//  Created by Matthew Chukwuemeka on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LegoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
